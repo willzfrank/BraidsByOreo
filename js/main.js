d54f4a2ffe62b5e3f569907a4ff38ffca78555a6
Godswill Francis <willzudoh4@gmail.com>
2023-03-31 19:57:52 -0700

    fixed all booking links and mobile navbar

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@
 const currentYear = new Date().getFullYear();
 const currentYearSpan = document.getElementById('currentYear');
 currentYearSpan.innerHTML = currentYear;
+
+// mobile navbar
+
+const menuButton = document.querySelector('.mobile__nav i.uil-bars');
+const closeButton = document.querySelector('.mobile__nav i.uil-times');
+const mainView = document.querySelector('.mobile__nav .navMobile__mainView');
+
+menuButton.addEventListener('click', function () {
+  mainView.classList.add('show');
+});
+
+closeButton.addEventListener('click', function () {
+  mainView.classList.remove('show');
+});
